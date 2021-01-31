a2503246fa1cdd9449f694876b9ef98d7db9ff83
alisonchou <alchousr21@herricksk12.org>
2021-01-31 15:54:31 -0500

    update my reviews after creating review

--- src/hooks/useCreateReview.js
+++ src/hooks/useCreateReview.js
@@ -1,14 +1,17 @@
 import { useMutation } from '@apollo/react-hooks';
 import { useHistory } from 'react-router-native';
 import { CREATE_REVIEW } from '../graphql/mutations';
+import useUser from './useUser';
 
 const useCreateReview = () => {
     const history = useHistory();
     const [mutate] = useMutation(CREATE_REVIEW);
+    const { refetch } = useUser({ includeReviews: true });
 
     return async newReview => {
         try {
             const { data } = await mutate({ variables: { ...newReview, rating: Number(newReview.rating) }});
+            await refetch();
             history.push(`repository/${data.createReview.repositoryId}`);
             return data;
         } catch (e) {
